f974112f38ba7d760d8b39eafda409341936886b
Neeraj P Yetheendran <46917698+NXPY123@users.noreply.github.com>
2022-08-14 09:07:11 +0530

    Update spotipy_token.py

--- spotipy_token.py
+++ spotipy_token.py
@@ -4,10 +4,10 @@ import yaml
 
 stream = open('config.yaml')
 user_config = yaml.load(stream, Loader=yaml.FullLoader)
-token = util.prompt_for_user_token(user_config['username'], 
+token = util.prompt_for_user_token('0aawyoae0w7swyq8m3op5276n', 
         scope='playlist-read-private', 
-        client_id=user_config['client_id'], 
-        client_secret=user_config['client_secret'], 
-        redirect_uri=user_config['redirect_uri'])
+        client_id='e9f8bbbe8e044a4f9db3516ebea16dd1', 
+        client_secret='4c267f7c2fe64054a2cbe2ee868f03c5', 
+        redirect_uri='http://localhost:9000/')
 print(token)
 spotipy.Spotify(auth=token)
